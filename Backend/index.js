0a31c39b2e05f10789374279c150da51406d4b59
UZAIR HAKEEM <151864936+Uzair73@users.noreply.github.com>
2024-02-02 11:10:49 +0500

    Update index.js
    update core

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,13 @@ connecttoMongoose();
 
 const express = require('express')
 const app = express()
-app.use(cors()) //use cores to fetch the data in the browser
+app.use(cors(
+  {
+  origin: ["https://todo-mern-app-bakend.vercel.app"],
+  methods: ["Get","POST","PUT","DELETE"],
+  credentials: true
+  }
+)) //use cores to fetch the data in the browser
 const port = 8080
 //req to server using json
 app.use(express.json())
