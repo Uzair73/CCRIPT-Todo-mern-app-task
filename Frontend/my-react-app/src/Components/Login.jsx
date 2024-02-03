1627329e19e013e3e85b8f7f89f6495c1a6156dc
UZAIR HAKEEM <151864936+Uzair73@users.noreply.github.com>
2024-02-03 20:59:35 +0500

    Update Login.jsx
    Change fetch api

--- Frontend/my-react-app/src/Components/Login.jsx
+++ Frontend/my-react-app/src/Components/Login.jsx
@@ -7,7 +7,7 @@ const Login = (props) => {
   const submitform = async (e) => {
     e.preventDefault();
     // API call
-    const response = await fetch(`http://localhost:8080/api/auth/login`, {
+    const response = await fetch(`https://ccript-todo-mern-app-task.vercel.app/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
