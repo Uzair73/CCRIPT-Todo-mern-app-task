1eb52572f4cfdd00e9740c30753ab6ec01a256da
UZAIR HAKEEM <151864936+Uzair73@users.noreply.github.com>
2024-02-05 11:49:11 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,13 +6,7 @@ connecttoMongoose();
 
 const express = require('express')
 const app = express()
-app.use(cors(
-  {
-  origin: [https://ccript-todo-mern-app-task.vercel.app/"],
-  methods: ["GET","POST","PUT","DELETE"],
-  credentials: true
-  }
-)) //use cores to fetch the data in the browser
+app.use(cors()) //use cores to fetch the data in the browser
 const port = 8080
 //req to server using json
 app.use(express.json())
